2402078ae126142cc1bde508a0ac7509cfb56567
Fernando Basilio <fernando.basilio@objective.com.br>
2022-12-05 09:28:54 -0300

    aula 5

--- Aula05/sistema.js
+++ Aula05/sistema.js
@@ -1,12 +1,48 @@
 
 const output = document.querySelector(".output");
+const txtColuna = document.getElementById("txtColuna");
+const txtLinha = document.getElementById("txtLinha");
+const txtFigura = document.getElementById("txtFigura");
 
-function start(comando, valorA, valorB){
+let matriz  = [
+    ['_', '_', '_'],
+    ['_', '_', '_'],
+    ['_', '_', '_'],
+];
+
+retorno = desenharTabuleiro(matriz);
+ouput(retorno);
+
+function start(){
     let retorno = 0;
+    let linha = txtLinha.value;
+    let coluna = txtColuna.value;
+    let figura = txtFigura.value;
+
+    matriz = desenharJogada(matriz, linha, coluna, figura);
+
+    retorno = desenharTabuleiro(matriz);
 
     ouput(retorno);
 }
 
 function ouput(texto){
-    output.innerHTML = texto;
+    output.innerHTML = '<br>'+texto;
+}
+
+function desenharJogada(matriz, linha, coluna, figura){
+
+    matriz[linha][coluna]=figura;
+    return matriz;
+}
+
+function desenharTabuleiro(matriz){
+    let tabuleiro="";
+    for(linha=0; linha <=2; linha++){
+        for(coluna=0; coluna <=2; coluna++){
+            tabuleiro = tabuleiro + '[' + matriz[linha][coluna]+'] ';
+        }
+        tabuleiro = tabuleiro + ' <br>';
+    }
+    return tabuleiro;
 }
